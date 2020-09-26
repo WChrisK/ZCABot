adfef884db8ad252e97432f72a8dd12e3e1e7d1a
Chris <38708476+WChrisK@users.noreply.github.com>
2020-09-26 14:40:48 -0400

    Added warning for mistyped command

--- Bot.cs
+++ Bot.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
@@ -10,12 +11,14 @@ namespace ZCABot
 {
     public partial class Bot
     {
+        private const char CommandCharacter = '.';
         private const string WelcomeMessage = "Welcome to the ZCA! You have been assigned any join roles, which you may remove by interacting with this bot. For help, type `.help` to me!";
         private const string DebugChannelName = "bot-logs";
         private const long GuildID = 480611202679177216;
         private const ulong ManagerRoleID = 480909038108934195;
         private const ulong StaffRoleID = 480911020827869194;
         private static readonly string[] JoinRoleNames = { "TeamGame", "Practice", "Duel", "Activation pending" };
+        private static Regex UnknownCommandRegex = new Regex(@"\.\w\w.+");
 
         private readonly DiscordSocketClient client;
 
@@ -162,12 +165,26 @@ namespace ZCABot
             if (tokens.Count <= 0)
                 return;
 
+            if (tokens[0].Length == 0 || tokens[0][0] != CommandCharacter)
+                return;
+
             switch (tokens[0].Substring(1).ToUpper())
             {
             case "HIGHLIGHT":
                 HandleHighlight(tokens, msg, msg.Author, channel);
                 break;
+            default:
+                HandleUnexpectedCommand(tokens[0], channel);
+                break;
             }
+        }
+
+        private void HandleUnexpectedCommand(string token, ITextChannel channel)
+        {
+            if (channel.Id != HighlightChannelID || !UnknownCommandRegex.IsMatch(token))
+                return;
+
+            channel.SendMessageAsync($"Unknown command '{token}'");
         }
 
         private void HandleDirectMessage(SocketMessage msg, IDMChannel channel)
